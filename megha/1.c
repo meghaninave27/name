a32e5180e5dafaadea455a464aaa041e665429f8
meghaninave27 <meghaninave8@gmail.com>
2024-10-03 18:38:15 +0530

    Add files via upload

---
+++ megha/1.c
@@ -0,0 +1,165 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i,j;
+	for(i=1;i<=5;i++)
+	{
+		for(j=1;j<=5;j++)
+		{
+			if(j==1)
+			{
+				printf("* ");	
+			}
+			else if(i==2 && j==2)
+			{
+				printf("* ");
+			}
+			else if(i==3 && j==3)
+			{
+				printf("* ");
+			}
+			else if(i==2 && j==4)
+			{
+				printf("* ");
+			}
+			else if(j==5)
+			{
+				printf("* ");
+			}
+			else
+			{
+				printf("  ");
+			}
+			
+		
+			
+		
+		}
+		printf("\n");
+	}
+	printf("\n");
+	for(i=1;i<=5;i++)
+	{
+		for(j=1;j<=5;j++)
+		{
+			if(i==1)
+			{
+				printf("* ");
+			}
+			else if (j==1)
+			{
+				printf("* ");
+				
+			}
+			else if (i==5)
+			{
+				printf("* ");
+				
+			}
+			else if (i==3)
+			{
+				printf("* ");
+				
+			}
+			else
+			{
+				printf("  ");
+			}
+		}
+		printf("\n");
+		
+	}
+	printf("\n");
+	for(i=1;i<=5;i++)
+	{
+		for(j=1;j<=5;j++)
+		{
+			if(i==1)
+			{
+				printf("* ");
+			}
+			else if(j==1)
+			{
+				printf("* ");
+			}
+				else if(i==5)
+			{
+				printf("* ");
+			}
+			else if(i==4 && j==5)
+			{
+				printf("* ");
+			}
+			else if(i==3 && j==5)
+			{
+				printf("* ");
+			}
+				else if(i==3 && j==4)
+			{
+				printf("* ");
+			}
+				else if(i==3 && j==3)
+			{
+				printf("* ");
+			}
+			else
+			{
+				printf("  ");
+			}
+		}
+		printf("\n");
+	}
+	printf("\n");
+	for(i=1;i<=5;i++)
+	{
+		for(j=1;j<=5;j++)
+		{
+			if(j==1)
+			{
+				printf("* ");
+			}
+			else if(j==5)
+			{
+				printf("* ");
+			}
+				else if(i==3)
+			{
+				printf("* ");
+			}
+			else
+			{
+				printf("  ");
+			}
+		}
+		printf("\n");
+	}
+	printf("\n");
+	for(i=1;i<=5;i++)
+	{
+		for(j=1;j<=5;j++)
+		{
+			if(i==1)
+			{
+				printf("* ");
+			}
+			else if(j==1)
+			{
+				printf("* ");
+			}
+			else if(j==5)
+			{
+				printf("* ");
+			}
+			else if(i==3)
+			{
+				printf("* ");
+			}
+			else
+			{
+				printf("  ");
+			}
+		}
+		printf("\n");
+	}
+}
